38bac73ea7dfef99e6955dc38780a65a605f1397
Saloni <salonimehta2127@gmail.com>
2021-07-22 23:09:07 -0400

    added show hogs and hide hogs toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Main from "./Main";
 
 import hogs from "../porkers_data";
 
 function App() {
+  const [showThePigs, setShowPigs] = useState(false)
+
   return (
     <div className="App">
       <Nav />
+      <div>
+        <button onClick={() => setShowPigs(!showThePigs)}>{showThePigs ? "Hide Hogs" : "Show Hogs"}</button>
+      </div>
+      {showThePigs ? <Main hogsInfo={hogs} /> : null}
 
-      <Main hogsInfo={hogs} />
     </div>
   );
 }
